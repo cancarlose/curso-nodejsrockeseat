771e772e494bcabb9431812ee731ee33a8a7322f
Carlos Cavalcanti <carlosedlima4@gmail.com>
2023-12-19 10:45:42 -0300

    Desafio 01 proposto

---
+++ desafio 01/server2.js
@@ -0,0 +1,30 @@
+import http from 'node:http'
+
+import { json } from '../src/middlewares/json.js'
+import { routes } from './routes2.js'
+import { extractQueryParams } from '../src/utils/extract-query-params.js'
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
+
+  await json(req, res)
+
+  const route = routes.find(route => {
+    return route.method === method && route.path.test(url)
+  })
+
+  if (route) {
+    const routeParams = req.url.match(route.path)
+
+    const { query, ...params } = routeParams.groups
+
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
+
+    return route.handler(req, res)
+  }
+
+  return res.writeHead(404).end()
+})
+
+server.listen(3335)
